b6ba79112eb0641241198c495f696834e4540518
bwlewis <blewis@illposed.net>
2016-07-01 22:11:04 -0400

    irlba

--- inst/examples/irlba.R
+++ inst/examples/irlba.R
@@ -1,35 +1,23 @@
 library(irlba)
 library(ddR)
 
-### Utility functions
-
-# Compute a %*% x where a is a darray and x is a vector
-mvec = function(a, x)
-{
-  collect(
-    dmapply(function(x, y) x %*% y,
-            parts(a),
-            replicate(totalParts(a), x, FALSE),
-            output.type="darray", combine="rbind", nparts=nparts(a)))
-}
-# Compute x %*% a where a is a darray and x is a vector
-vecm = function(x, a)
-{
-  colSums(
-    dmapply(function(x, y) x %*% y,
-            split(x, rep(1:nparts(a)[1], psize(a)[, 1])),
-            parts(a),
-            output.type="darray", combine="rbind", nparts=nparts(a)))
-}
 setMethod("%*%", signature(x="ParallelObj", y="numeric"), function(x ,y)
 {
   stopifnot(ncol(x) == length(y))
-  mvec(x, y)
+  collect(
+    dmapply(function(a, b) a %*% b,
+            parts(x),
+            replicate(totalParts(x), y, FALSE),
+            output.type="darray", combine="rbind", nparts=nparts(x)))
 })
 setMethod("%*%", signature(x="numeric", y="ParallelObj"), function(x ,y)
 {
   stopifnot(length(x) == nrow(y))
-  vecm(x, y)
+  colSums(
+    dmapply(function(x, y) x %*% y,
+            split(x, rep(1:nparts(y)[1], psize(y)[, 1])),
+            parts(y),
+            output.type="darray", combine="rbind", nparts=nparts(y)))
 })
 
 x = dmapply(function(x) matrix(runif(4), 25, 25), 1:4, output.type="darray", combine="rbind", nparts=c(4, 1))
